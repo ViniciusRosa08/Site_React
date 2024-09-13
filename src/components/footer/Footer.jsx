7b3e807fea98d7d43049debeb6f6ad74cf4b0d69
ViniciusRosa08 <126347927+ViniciusRosa08@users.noreply.github.com>
2024-09-13 19:26:56 -0300

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Vinicius Rosa | DSM 4°SEM</p>
+    </footer>
+  )
+}
+
+export default Footer
